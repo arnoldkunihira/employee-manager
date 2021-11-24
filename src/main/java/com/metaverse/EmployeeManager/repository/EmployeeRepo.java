d5dd0741fa5e0d7322c7bf4d475906e10bbe5c47
Arnold Kunihira <arnold.kmulera@gmail.com>
2021-11-24 07:54:00 +0300

    Add an employee interface for abstraction

--- src/main/java/com/metaverse/EmployeeManager/repository/EmployeeRepo.java
+++ src/main/java/com/metaverse/EmployeeManager/repository/EmployeeRepo.java
@@ -1,2 +1,12 @@
-package com.metaverse.EmployeeManager.repository;public interface EmployeeRepo {
+package com.metaverse.EmployeeManager.repository;
+
+import com.metaverse.EmployeeManager.models.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
+
+    Optional<Employee> findEmployeeById(Long id);
 }
