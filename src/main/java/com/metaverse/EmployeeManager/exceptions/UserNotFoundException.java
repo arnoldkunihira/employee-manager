fa949f881303c8bbc586f3edde0f92b8154b4de1
Arnold Kunihira <arnold.kmulera@gmail.com>
2021-11-26 01:07:38 +0300

    Add user exceptions thrown from normal operations

--- src/main/java/com/metaverse/EmployeeManager/exceptions/UserNotFoundException.java
+++ src/main/java/com/metaverse/EmployeeManager/exceptions/UserNotFoundException.java
@@ -1,4 +1,4 @@
-package com.metaverse.EmployeeManager.exception;
+package com.metaverse.EmployeeManager.exceptions;
 
 public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(String message) {
