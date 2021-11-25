bbc361f04164530c714d4a313067a4ce782017d9
Arnold Kunihira <arnold.kmulera@gmail.com>
2021-11-26 01:02:49 +0300

    Add and modify Employee repository

--- src/main/java/com/metaverse/EmployeeManager/repository/EmployeeRepo.java
+++ src/main/java/com/metaverse/EmployeeManager/repository/EmployeeRepo.java
@@ -2,11 +2,16 @@ package com.metaverse.EmployeeManager.repository;
 
 import com.metaverse.EmployeeManager.models.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface EmployeeRepo extends JpaRepository<Employee, Long> {
-    void deleteEmployeeById(Long id);
 
     Optional<Employee> findEmployeeById(Long id);
+
+    @Query("SELECT e FROM Employee e WHERE e.email = ?1")
+    Optional<Employee> findEmployeeByEmail(String email);
 }
